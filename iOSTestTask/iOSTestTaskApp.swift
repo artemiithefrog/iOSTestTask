85c3ee6e184fd5394f5d66523ff0cd8dcabcfc93
Artemii Malyshev <115695294+artemiithefrog@users.noreply.github.com>
2024-06-16 11:43:40 +0200

    Initial Commit

---
+++ iOSTestTask/iOSTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTestTaskApp.swift
+//  iOSTestTask
+//
+//  Created by artemiithefrog . on 16.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct iOSTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
